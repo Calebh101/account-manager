4dc6af25c34b945b1e12fa9f1dff0eef856aac6f
caleb <calebh101dev@icloud.com>
2026-05-04 08:07:58 -0500

    Update dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Calebh101 Account',
       theme: ThemeData.light(),
       darkTheme: ThemeData.dark(),
       debugShowCheckedModeBanner: false,
