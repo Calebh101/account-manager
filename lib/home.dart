15c66eaea314211f12add7940d68d7815c1ea9dd
caleb <calebh101dev@icloud.com>
2026-05-04 20:30:32 -0500

    Cool stuff tho yay

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,7 @@
 import 'package:calebh101_account_page/main.dart';
 import 'package:calebh101_account_page/verify_email.dart';
 import 'package:calebh101_server/calebh101_server.dart';
+import 'package:calebh101_server_flutter/calebh101_server_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:localpkg_flutter/localpkg.dart';
 
@@ -18,7 +19,9 @@ class _HomeState extends State<Home> {
   AccountDetailsPost200ResponseData? data;
 
   Future<void> fetch() async {
-    onNeedsLogin = (e) {};
+    onNeedsLogin = (e) async {
+      SimpleNavigator.navigate(context: context, page: LoginPage(client: client), mode: NavigatorMode.pushReplacement);
+    };
 
     final result = await request(() => api.accountDetailsPost());
     if (result == null) return;
@@ -32,16 +35,22 @@ class _HomeState extends State<Home> {
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Column(
+          child: data != null ? (Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text("This application is not finished yet.", softWrap: true),
-              Text("To verify your email with Calebh101 Services, press the button below.", softWrap: true),
+              Text("Received data: ${data.runtimeType}", softWrap: true),
+            ],
+          )) : (error != null ? Text("Error: $error") : Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: CircularProgressIndicator(),
+              ),
               TextButton(onPressed: () {
-                SimpleNavigator.navigate(context: context, page: VerifyEmail(), mode: NavigatorMode.pushReplacement);
+                SimpleNavigator.navigate(context: context, page: VerifyEmail(), mode: NavigatorMode.push);
               }, child: Text("Verify Email")),
             ],
-          ),
+          )),
         ),
       ),
     );
